13a686bc9ac501ba73114b0c410598bdac9afd51
Amirabdela <alhelili451@gmail.com>
2025-05-19 16:07:50 +0300

    commited

--- about.php
+++ about.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/includes/header.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
+<!-- about pag -->
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
